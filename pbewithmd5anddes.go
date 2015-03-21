aabdbc45f9acdb74546f3e37d1a9e20a24be8b8b
PotatoDumplings <lucas@streak.com>
2015-03-21 15:33:34 -0700

    Now supports encryption.

--- pbewithmd5anddes.go
+++ pbewithmd5anddes.go
@@ -8,17 +8,45 @@ import (
     "crypto/cipher"
     "crypto/des"
     "crypto/md5"
+    "crypto/rand"
 )
 
 func getDerivedKey(password string, salt string, count int) ([]byte, []byte) {
     key := md5.Sum([]byte(password + salt))
-    for i:= 0; i < count - 1; i++ {
+    for i := 0; i < count - 1; i++ {
         key = md5.Sum(key[:])
     }
     return key[:8], key[8:]
 }
 
-func DecryptString(password string, obtenationiterations int, cipherText string) (string, error) {
+func Encrypt(password string, obtenationIterations int, plainText string) (string, error) {
+    salt := make([]byte, 8)
+    _, err := rand.Read(salt)
+	if err != nil {
+		return "", err
+	}
+
+    padNum := byte(8 - len(plainText) % 8)
+    for i := byte(0); i < padNum; i++ {
+        plainText += string(padNum)
+    }
+
+    dk, iv := getDerivedKey(password, string(salt), obtenationIterations)
+
+    block,err := des.NewCipher(dk)
+
+    if err != nil {
+        return "", err
+    }
+
+    encrypter := cipher.NewCBCEncrypter(block, iv)
+    encrypted := make([]byte, len(plainText))
+    encrypter.CryptBlocks(encrypted, []byte(plainText))
+
+    return base64.StdEncoding.EncodeToString(append(salt, encrypted...)), nil
+}
+
+func Decrypt(password string, obtenationIterations int, cipherText string) (string, error) {
     msgBytes, err := base64.StdEncoding.DecodeString(cipherText)
     if err != nil {
         return "", err
@@ -27,7 +55,7 @@ func DecryptString(password string, obtenationiterations int, cipherText string)
     salt := msgBytes[:8]
     encText := msgBytes[8:]
 
-    dk, iv := getDerivedKey(password, string(salt), obtenationiterations)
+    dk, iv := getDerivedKey(password, string(salt), obtenationIterations)
     block,err := des.NewCipher(dk)
 
     if err != nil {
--- pbewithmd5anddes_test.go
+++ pbewithmd5anddes_test.go
@@ -16,12 +16,48 @@ func TestBasicDecryption(t *testing.T) {
 		{"4v7gZN8/e20qX7Nm5EVbRs84zZ7IkWt+GNi8q+4dETeJodVONdoF7jaXBl8qialZ5KIlvlDD04idlAVjqiY6H/HDxkWBcyTE", "g9Q95=yNVt7E?a+nDN=%", "ðƏ kwɪk braʊn fƊks dʒʊmptƏʊvƏ ðƏ lɛɪzi: dƊgz"},
 	}
 	for _, c := range cases {
-		got, err := DecryptString(c.password, 1000, c.ciphertext)
+		got, err := Decrypt(c.password, 1000, c.ciphertext)
 		if err != nil {
 			t.Errorf("Got error %q for password %q, ciphertext %q", err.Error(), c.password, c.ciphertext)
 		}
 		if got != c.plaintext {
-			t.Errorf("DecryptString(%q, 1000, %q) == %q, want %q", c.password, c.ciphertext, got, c.plaintext)
+			t.Errorf("Decrypt(%q, 1000, %q) == %q, want %q", c.password, c.ciphertext, got, c.plaintext)
+		}
+	}
+}
+
+func TestBasicEncryption(t *testing.T) {
+	cases := []struct {
+		plaintext string
+		password string
+		iterations int
+	}{
+		{"plaintext", "password", 1000},
+		{"Lorem ipsum dolor sit amet, consectetur adipiscing elit.", "password", 1000},
+		{"", "password", 1000},
+		{"ðƏ kwɪk braʊn fƊks dʒʊmptƏʊvƏ ðƏ lɛɪzi: dƊgz", "password", 1000},
+		{"plaintext", "g9Q95=yNVt7E?a+nDN=%", 1000},
+		{"Lorem ipsum dolor sit amet, consectetur adipiscing elit.", "g9Q95=yNVt7E?a+nDN=%", 1000},
+		{"", "g9Q95=yNVt7E?a+nDN=%", 1000},
+		{"ðƏ kwɪk braʊn fƊks dʒʊmptƏʊvƏ ðƏ lɛɪzi: dƊgz", "g9Q95=yNVt7E?a+nDN=%", 1000},
+		{"plaintext", "password", 5},
+		{"Lorem ipsum dolor sit amet, consectetur adipiscing elit.", "password", 5},
+		{"", "password", 5},
+		{"ðƏ kwɪk braʊn fƊks dʒʊmptƏʊvƏ ðƏ lɛɪzi: dƊgz", "password", 5},
+		{"plaintext", "g9Q95=yNVt7E?a+nDN=%", 5},
+		{"Lorem ipsum dolor sit amet, consectetur adipiscing elit.", "g9Q95=yNVt7E?a+nDN=%", 5},
+		{"", "g9Q95=yNVt7E?a+nDN=%", 5},
+		{"ðƏ kwɪk braʊn fƊks dʒʊmptƏʊvƏ ðƏ lɛɪzi: dƊgz", "g9Q95=yNVt7E?a+nDN=%", 5},
+	}
+	for _, c := range cases {
+		ciphertext, err := Encrypt(c.password, c.iterations, c.plaintext)
+		if err != nil {
+			t.Errorf("Got error %q for password %q, plaintext %q", err.Error(), c.password, c.plaintext)
+		}
+
+		plaintext, err := Decrypt(c.password, c.iterations, ciphertext)
+		if plaintext != c.plaintext {
+			t.Errorf("Got %q, want %q", plaintext, c.plaintext)
 		}
 	}
 }
