e7423a5a739760385729a2098b373b47bbf7d25a
Marcelo Gomes Jr <2008155+cll0ud@users.noreply.github.com>
2019-07-20 17:01:11 -0300

    Added helper functions

--- pbewithmd5anddes.go
+++ pbewithmd5anddes.go
@@ -20,11 +20,31 @@ func getDerivedKey(password string, salt string, count int) ([]byte, []byte) {
 }
 
 func Encrypt(password string, obtenationIterations int, plainText string) (string, error) {
+    return doEncrypt(password, plainText, "", obtenationIterations)
+}
+
+func Decrypt(password string, obtenationIterations int, cipherText string) (string, error) {
+    return doDecrypt(password, cipherText, "", obtenationIterations)
+}
+
+func EncryptWithFixedSalt(password string, obtenationIterations int, plainText string, fixedSalt string) (string, error) {
+    return doEncrypt(password, plainText, fixedSalt, obtenationIterations)
+}
+
+func DecryptWithFixedSalt(password string, obtenationIterations int, cipherText string, fixedSalt string) (string, error) {
+    return doDecrypt(password, cipherText, fixedSalt, obtenationIterations)
+}
+
+func doEncrypt(password, plainText, fixedSalt string, obtenationIterations int) (string, error) {
     salt := make([]byte, 8)
-    _, err := rand.Read(salt)
-	if err != nil {
-		return "", err
-	}
+    if fixedSalt == "" {
+    	_, err := rand.Read(salt)
+        if err != nil {
+            return "", err
+        }
+    } else {
+        copy(salt[:], fixedSalt)
+    }
 
     padNum := byte(8 - len(plainText) % 8)
     for i := byte(0); i < padNum; i++ {
@@ -43,77 +63,36 @@ func Encrypt(password string, obtenationIterations int, plainText string) (strin
     encrypted := make([]byte, len(plainText))
     encrypter.CryptBlocks(encrypted, []byte(plainText))
 
-    return base64.StdEncoding.EncodeToString(append(salt, encrypted...)), nil
+    if fixedSalt == "" {
+        return base64.StdEncoding.EncodeToString(append(salt, encrypted...)), nil
+    } else {
+        return base64.StdEncoding.EncodeToString(encrypted), nil
+    }
 }
 
-func Decrypt(password string, obtenationIterations int, cipherText string) (string, error) {
-    msgBytes, err := base64.StdEncoding.DecodeString(cipherText)
-    if err != nil {
-        return "", err
-    }
-
-    salt := msgBytes[:8]
-    encText := msgBytes[8:]
-
-    dk, iv := getDerivedKey(password, string(salt), obtenationIterations)
-    block,err := des.NewCipher(dk)
-
-    if err != nil {
-        return "", err
-    }
-
-    //decrypt
-    decrypter := cipher.NewCBCDecrypter(block, iv)
-    decrypted := make([]byte, len(encText))
-    decrypter.CryptBlocks(decrypted, encText)
-
-    decryptedString := strings.TrimRight(string(decrypted), "\x01\x02\x03\x04\x05\x06\x07\x08")
-
-    return decryptedString, nil
-}
-
-func EncryptWithFixedSalt(password string, obtenationIterations int, plainText string, fixedSalt string) (string, error) {
-    salt := make([]byte, 8)
-    copy(salt[:], fixedSalt)
-
-    padNum := byte(8 - len(plainText) % 8)
-    for i := byte(0); i < padNum; i++ {
-       plainText += string(padNum)
-    }
-
-    dk, iv := getDerivedKey(password, string(salt), obtenationIterations)
-
-    block,err := des.NewCipher(dk)
-
-    if err != nil {
-        return "", err
-    }
-
-    encrypter := cipher.NewCBCEncrypter(block, iv)
-    encrypted := make([]byte, len(plainText))
-    encrypter.CryptBlocks(encrypted, []byte(plainText))
-
-    return base64.StdEncoding.EncodeToString(encrypted), nil
-}
-
-func DecryptWithFixedSalt(password string, obtenationIterations int, cipherText string, fixedSalt string) (string, error) {
+func doDecrypt(password, cipherText, fixedSalt string, obtenationIterations int) (string, error) {
     msgBytes, err := base64.StdEncoding.DecodeString(cipherText)
     if err != nil {
         return "", err
     }
 
     salt := make([]byte, 8)
-    copy(salt[:], fixedSalt)
-    encText := msgBytes[:]
+    var encText []byte
+    if fixedSalt == "" {
+        salt = msgBytes[:8]
+        encText = msgBytes[8:]
+    } else {
+        copy(salt[:], fixedSalt)
+        encText = msgBytes[:]
+    }
 
     dk, iv := getDerivedKey(password, string(salt), obtenationIterations)
-    block,err := des.NewCipher(dk)
+    block, err := des.NewCipher(dk)
 
     if err != nil {
         return "", err
     }
 
-    //decrypt
     decrypter := cipher.NewCBCDecrypter(block, iv)
     decrypted := make([]byte, len(encText))
     decrypter.CryptBlocks(decrypted, encText)
