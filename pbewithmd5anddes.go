1ae53f47cfcaa94d217b88b8de711ba214811cc9
Marcelo Gomes Jr <marcelo.gomes.junior@gmail.com>
2019-07-20 05:41:09 -0300

    Added support for fixed salt encryption/decryption

--- pbewithmd5anddes.go
+++ pbewithmd5anddes.go
@@ -71,3 +71,54 @@ func Decrypt(password string, obtenationIterations int, cipherText string) (stri
 
     return decryptedString, nil
 }
+
+func EncryptWithFixedSalt(password string, obtenationIterations int, plainText string, fixedSalt string) (string, error) {
+    salt := make([]byte, 8)
+    copy(salt[:], fixedSalt)
+
+    padNum := byte(8 - len(plainText) % 8)
+    for i := byte(0); i < padNum; i++ {
+       plainText += string(padNum)
+    }
+
+    dk, iv := getDerivedKey(password, string(salt), obtenationIterations)
+
+    block,err := des.NewCipher(dk)
+
+    if err != nil {
+        return "", err
+    }
+
+    encrypter := cipher.NewCBCEncrypter(block, iv)
+    encrypted := make([]byte, len(plainText))
+    encrypter.CryptBlocks(encrypted, []byte(plainText))
+
+    return base64.StdEncoding.EncodeToString(encrypted), nil
+}
+
+func DecryptWithFixedSalt(password string, obtenationIterations int, cipherText string, fixedSalt string) (string, error) {
+    msgBytes, err := base64.StdEncoding.DecodeString(cipherText)
+    if err != nil {
+        return "", err
+    }
+
+    salt := make([]byte, 8)
+    copy(salt[:], fixedSalt)
+    encText := msgBytes[:]
+
+    dk, iv := getDerivedKey(password, string(salt), obtenationIterations)
+    block,err := des.NewCipher(dk)
+
+    if err != nil {
+        return "", err
+    }
+
+    //decrypt
+    decrypter := cipher.NewCBCDecrypter(block, iv)
+    decrypted := make([]byte, len(encText))
+    decrypter.CryptBlocks(decrypted, encText)
+
+    decryptedString := strings.TrimRight(string(decrypted), "\x01\x02\x03\x04\x05\x06\x07\x08")
+
+    return decryptedString, nil
+}
