8df6083a134b04bff64cb6d149a79d0d3017587b
Marcelo Gomes Jr <2008155+cll0ud@users.noreply.github.com>
2019-07-20 20:35:19 -0300

    Moved if logic to specific functions instead of doEncrypt/doDecrypt also fixed indentation

--- pbewithmd5anddes.go
+++ pbewithmd5anddes.go
@@ -20,72 +20,74 @@ func getDerivedKey(password string, salt string, count int) ([]byte, []byte) {
 }
 
 func Encrypt(password string, obtenationIterations int, plainText string) (string, error) {
-    return doEncrypt(password, plainText, "", obtenationIterations)
+	salt := make([]byte, 8)
+	_, err := rand.Read(salt)
+	if err != nil {
+		return "", err
+	}
+
+	encText, err := doEncrypt(password, plainText, salt, obtenationIterations)
+	if err != nil {
+		return "", err
+	}
+
+	return base64.StdEncoding.EncodeToString(append(salt, encText...)), nil
 }
 
 func Decrypt(password string, obtenationIterations int, cipherText string) (string, error) {
-    return doDecrypt(password, cipherText, "", obtenationIterations)
+	msgBytes, err := base64.StdEncoding.DecodeString(cipherText)
+	if err != nil {
+		return "", err
+	}
+
+	salt := msgBytes[:8]
+	encText := msgBytes[8:]
+	return doDecrypt(password, encText, salt, obtenationIterations)
 }
 
 func EncryptWithFixedSalt(password string, obtenationIterations int, plainText string, fixedSalt string) (string, error) {
-    return doEncrypt(password, plainText, fixedSalt, obtenationIterations)
+	salt := make([]byte, 8)
+	copy(salt[:], fixedSalt)
+
+	encText, err := doEncrypt(password, plainText, salt, obtenationIterations)
+	if err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(encText), nil
 }
 
 func DecryptWithFixedSalt(password string, obtenationIterations int, cipherText string, fixedSalt string) (string, error) {
-    return doDecrypt(password, cipherText, fixedSalt, obtenationIterations)
+	msgBytes, err := base64.StdEncoding.DecodeString(cipherText)
+	if err != nil {
+		return "", err
+	}
+
+	salt := make([]byte, 8)
+	copy(salt[:], fixedSalt)
+	encText := msgBytes[:]
+	return doDecrypt(password, encText, salt, obtenationIterations)
 }
 
-func doEncrypt(password, plainText, fixedSalt string, obtenationIterations int) (string, error) {
-    salt := make([]byte, 8)
-    if fixedSalt == "" {
-    	_, err := rand.Read(salt)
-        if err != nil {
-            return "", err
-        }
-    } else {
-        copy(salt[:], fixedSalt)
-    }
-
+func doEncrypt(password, plainText string, salt []byte, obtenationIterations int) ([]byte, error) {
     padNum := byte(8 - len(plainText) % 8)
     for i := byte(0); i < padNum; i++ {
         plainText += string(padNum)
     }
 
     dk, iv := getDerivedKey(password, string(salt), obtenationIterations)
-
     block,err := des.NewCipher(dk)
-
     if err != nil {
-        return "", err
+        return nil, err
     }
 
     encrypter := cipher.NewCBCEncrypter(block, iv)
     encrypted := make([]byte, len(plainText))
     encrypter.CryptBlocks(encrypted, []byte(plainText))
 
-    if fixedSalt == "" {
-        return base64.StdEncoding.EncodeToString(append(salt, encrypted...)), nil
-    } else {
-        return base64.StdEncoding.EncodeToString(encrypted), nil
-    }
+	return encrypted, nil
 }
 
-func doDecrypt(password, cipherText, fixedSalt string, obtenationIterations int) (string, error) {
-    msgBytes, err := base64.StdEncoding.DecodeString(cipherText)
-    if err != nil {
-        return "", err
-    }
-
-    salt := make([]byte, 8)
-    var encText []byte
-    if fixedSalt == "" {
-        salt = msgBytes[:8]
-        encText = msgBytes[8:]
-    } else {
-        copy(salt[:], fixedSalt)
-        encText = msgBytes[:]
-    }
-
+func doDecrypt(password string, encText, salt []byte, obtenationIterations int) (string, error) {
     dk, iv := getDerivedKey(password, string(salt), obtenationIterations)
     block, err := des.NewCipher(dk)
 
